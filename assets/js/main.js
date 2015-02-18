3bb365c929ad4dafbe02b3b20d41c92bbb347418
Konviser <svetsapelova@gmail.com>
2015-02-18 23:10:35 +0100

    workaround to make header disappear

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,19 +5,31 @@ $("window").load(function() {
 jQuery(document).ready(function($) {
 	if ($(window).width() > 768) {
 		var headerHeight = $('#header').height();
+		var handleScroll = true;
+
 		$(window).on('scroll', { previousScroll: 0 }, function () {
 			var currentScroll = $(this).scrollTop();
 			if (currentScroll > this.previousScroll) {
 				if (currentScroll > headerHeight) {
 					$('#header').slideUp(200);
 				}
-			} else {
+			} else if (handleScroll){
 				$('#header').slideDown(200);
+			}
+			else {
+				//a bit of workaround to make header disappear 
+				handleScroll = true;
 			}
 			this.previousScroll = currentScroll;
 		});
 	}
 	$('#disapear-on-click').on('click', function () {
 		$(this).hide();
-	});	
+	});
+
+	$('#header a').on('click', function(e){
+		handleScroll = false;
+		$('#header').slideUp(200);
+		
+	})	
 });
